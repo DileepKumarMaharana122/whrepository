20d28ae8e65fe8a5f7c9d9b7d9764c6e1ffbec49
User <User@SAMSUNG>
2018-05-09 12:06:33 +0530

    abcd

--- src/whrepository/TestGit.java
+++ src/whrepository/TestGit.java
@@ -4,5 +4,24 @@ public class TestGit {
 private String name;
 private String mark;
   private int value;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getMark() {
+	return mark;
+}
+public void setMark(String mark) {
+	this.mark = mark;
+}
+public int getValue() {
+	return value;
+}
+public void setValue(int value) {
+	this.value = value;
+}
+  
 
 }
